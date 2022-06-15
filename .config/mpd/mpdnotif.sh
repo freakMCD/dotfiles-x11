1db3cd3183fdbfae7d6121abc1476545fcc36e61
Edwin <vastagox@gmail.com>
2022-06-14 19:53:56 -0500

    improved dunst theme

--- .config/mpd/mpdnotif.sh
+++ .config/mpd/mpdnotif.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#form="<b>Artist</b>\t<span color='##a1b56c'>%artist%</span>\n<b>Album</b>\t<span color='##6a9fb5'>%album%</span>\n<b>Title</b>\t<span color='##ac4142'>%title%</span>" 
-form="\n<span color='##FFA348'>%artist%</span>\n<span color='##3584E4'>%album%</span>\n<span color='##57E389'>%title%</span>" 
+
+form="\n<span color='##FFA348'>%artist%</span>\n%title%\n<span color='##3584E4'>%album%</span>" 
+
+#form="\n<span color='##FFA348'>%artist%</span>\n<span color='##57E389'>%title%</span>\n<span color='##3584E4'>%album%</span>" 
+
 while "true"; do
    
     artpath="$HOME/Música/2015-2022/"$(mpc status -f '%file%' | head -n1)""
@@ -13,9 +16,9 @@ while "true"; do
     fi
 
     if [[ $status == 'playing' ]]; then
-	notify-send "♫ Now playing" "$(mpc --format "$form" current)" -i "$cover" -t 3000
+	notify-send " Now playing" "$(mpc --format "$form" current)" -i "$cover" -t 3000
     elif [[ $status == 'paused' ]]; then
-	notify-send  "Paused" "$(mpc --format "$form" current)" -i "$cover" -t 1000
+	notify-send  " Paused" "$(mpc --format "$form" current)" -i "$cover" -t 1000
     else 
 	notify-send "CODE WHEN WRONG"
     fi
