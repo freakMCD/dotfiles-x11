11cb225a5f410569ab7ee43c4eb5607812ae47a4
edwin <vastagox@gmail.com>
2022-06-29 17:40:49 -0500

    rule to higher icon size and centered text mpd notification

--- .config/dunst/launch_dunst.sh
+++ .config/dunst/launch_dunst.sh
@@ -18,6 +18,5 @@ DUNST_FILE=~/.config/dunst/dunstrc
 # update dunst based on pywal colors.
 sed -i '/background = /s/.*/    background = "'$color0'd9"/' $DUNST_FILE
 sed -i '/foreground = /s/.*/    foreground = "'$color6'"/' $DUNST_FILE
-sed -i '/frame_color = /s/.*/    frame_color = "'$color2'"/' $DUNST_FILE
 
 dunst -config ~/.config/dunst/dunstrc > /dev/null 2>&1 &
