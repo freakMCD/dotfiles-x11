3ec2c3634a12ffaa5ec4586de78158bd02f1f199
edwin <vastagox@gmail.com>
2022-12-25 04:15:14 -0500

    dunst config improved

--- .config/dunst/launch_dunst.sh
+++ .config/dunst/launch_dunst.sh
@@ -16,7 +16,10 @@ pidof dunst && killall dunst
 DUNST_FILE=~/.config/dunst/dunstrc
 
 # update dunst based on pywal colors.
+sed -i '/frame_color = /s/.*/    frame_color = "'$color3'"/' $DUNST_FILE
 sed -i '/background = /s/.*/    background = "'$color0'f7"/' $DUNST_FILE
 sed -i -E 's/span color=\o047#[0-Z]{6}/span color=\o047'$color4'/' $DUNST_FILE
+
+
 
 dunst -config ~/.config/dunst/dunstrc > /dev/null 2>&1 &
