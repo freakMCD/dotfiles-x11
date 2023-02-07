0fa37fbf5e88ae8bc0646fbacfb3bc2ca19e1247
edwin <vastagox@gmail.com>
2023-02-06 19:06:43 -0500

    chore: quickmarks, newsboat

--- .newsboat/newsnotif.sh
+++ .newsboat/newsnotif.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 click_action () {
-	i3-msg '[instance=newsboat] scratchpad show'
+	i3-msg '[instance=newsboat] focus'
 }
 
 if [[ "$*" != "0 unread articles" ]]; then
-	ACTION=$(dunstify -t 60000 -i ~/.newsboat/rss.png --action="default,Click" "newsboat" "$*")
-	case "$ACTION" in
-		"default")
- 	 	  click_action
-	  	;;
-	esac
+    ACTION=$(dunstify -t 60000 -i ~/.newsboat/rss.png --action="default,Click" "newsboat" "$*")
+    case "$ACTION" in
+	    "default")
+ 	 	    click_action
+	    ;;
+    esac
 fi
 
