232d3f06178797d08006d8a987fc88b191942319
edwin <vastagox@gmail.com>
2023-02-18 22:46:34 -0500

    BIG Chore maintenance

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,14 +84,6 @@ require("luasnip.loaders.from_vscode").lazy_load()
     })
   })
 
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
-
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
--- .config/qutebrowser/greasemonkey/reddit.user.js
+++ .config/qutebrowser/greasemonkey/reddit.user.js
@@ -3,7 +3,6 @@
 // @namespace    https://gist.github.com/bitraid/d1901de54382532a03b9b22a207f0417
 // @version      1.0
 // @description  reddit to teddit
-// @match        *://*.reddit.com/*
 // @match        *://reddit.com/*
 // @grant        none
 // @run-at       document-start
