3a859fedd601f20a1494e94e352ea7584d4d4927
edwin <vastagox@gmail.com>
2023-02-23 22:35:45 -0500

    "Change mpv, neovim configs"
    - MPV: Show osd-status-msg on seek
    - Neovim: Fixed nvim-cmp plugin configuration
    - Neovim: Clean init.lua file
    - Qutebrowser: Chore maintenance

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,8 +11,6 @@ return require('packer').startup(function(use)
 
     use 'lervag/vimtex'
 
-    use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/cmp-buffer'
 	use 'hrsh7th/cmp-path'
 	use 'hrsh7th/cmp-cmdline'
@@ -28,72 +26,121 @@ local custom_wal = require'lualine.themes.pywal'
 -- Change the background of lualine_b section for normal mode
 custom_wal.normal.b.bg = '#252525'
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 require('lualine').setup({
     options = { 
         theme = custom_wal,
     },
 })
 
-
-vim.opt.completeopt = "menu,menuone,noselect"
 -- Set up nvim-cmp.
-local cmp = require'cmp'
+vim.opt.completeopt = "menu,menuone,noselect"
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = ""
+}
+
 require("luasnip.loaders.from_vscode").lazy_load()
 
-  cmp.setup({
-    view = {                                                        
-      entries = {name = 'custom', selection_order = 'near_cursor' } 
-    },                                                               
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+cmp.setup({
+
+    formatting = {
+	  format = function(entry, vim_item)
+	    -- Kind icons
+	    vim_item.kind = string.format(' %s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+	   	return vim_item
+	  end
+	},
+    
+    snippet = {   -- REQUIRED
+      expand = function(args) 
+        require('luasnip').lsp_expand(args.body) 
       end,
     },
+
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
     },
+
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+        -- they way you will only jump inside the snippet region
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+  
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      }),
+
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
-      { name = 'buffer' },
+      { name = 'path'},
+      { name = 'nvim_lsp', keyword_length = 1},
+      { name = 'buffer', keyword_length = 3},
+      { name = 'luasnip', keyword_length = 2}, -- For luasnip users.
     })
   })
 
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-      { name = 'buffer' },
-    })
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
+  -- Use cmdline & path source for ':' 
+    cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
     })
   })
-
 
 vim.cmd([[
     colorscheme wal
