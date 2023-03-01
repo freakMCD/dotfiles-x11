9e2f1c9d689ad6a733e33a2f4a261f42c8711a6c
edwin <vastagox@gmail.com>
2023-02-28 20:51:36 -0500

    Network check status bar and if (1) start program

--- bin/startup/start_if_network_on.sh
+++ bin/startup/start_if_network_on.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 while "true"; do
-    if [[ $(nmcli networking connectivity check eth0) == 'full' ]]; then
+    if ping -c 10 -W 5 google.com 1>/dev/null 2>&1 
+    then
         urxvtc -name neomutt -geometry 80x25+297+1 -e neomutt
         urxvtc -name newsboat -geometry 80x25+297+1 -e newsboat
-        exit 0
+        break
     fi
     sleep 30
 done
